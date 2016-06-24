e22d185fb150a357b6011b85ae91536683b711e4
MLee21 <minnie.lee89@gmail.com>
2016-06-24 12:16:41 -0600

    added strong params for TasksController. can create a task for a project.

--- app/controllers/v1/tasks_controller.rb
+++ app/controllers/v1/tasks_controller.rb
@@ -2,9 +2,30 @@ module V1
   class TasksController < ApplicationController
     swagger_controller :tasks, 'Tasks'
 
+    swagger_api :create do 
+      summary 'Creates a new Task'
+      param :form, :name, :string, :required, 'Task designation'
+      param :form, :description, :string, :optional, 'Task description'
+    end
     def create
+      task = Task.new(task_params)
+      if task.save
+        render json: task, status: 201
+      else
+        render json: { errors: task.errors.full_messages }, status: 400
+      end
     end
 
+    private
+
+    def task_params
+      params.require(:task).permit(
+        :name,
+        :description,
+        :state,
+        :project_id
+      )
+    end
 
   end
 end
