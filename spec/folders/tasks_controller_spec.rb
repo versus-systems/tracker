b0a8b98d1b7f6da0a071f16035f15950d5e98112
MLee21 <minnie.lee89@gmail.com>
2016-06-24 12:07:47 -0600

    created spec for TasksController

---
+++ spec/folders/tasks_controller_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe V1::TasksController, type: :controller do 
+
+  describe "POST create" do 
+  let!(:project) {Project.create(name: "Project", description: "Cool")}
+    it "creates a task for a project" do 
+      parameters = {
+        project_id: project.id,
+        task: {
+          name: "Project",
+          description: "Description"
+        }
+      }
+      expect{post :create, parameters}.to change(Task, :count).by(1)
+      expect(response).to be_success
+    end
+  end
+
+  # describe "GET index" do 
+
+  #   it "gets all of the projects with their tasks" do 
+  #     project = Project.create!(name: "Project", description: "D")
+  #     get :index 
+  #     expect(response).to be_success
+  #     expect(Project.all.count).to eq(1)
+  #   end
+
+  # end
+end
